1750556dee89de764ce6ff1a446c5e2cc827ebb2
peterstefanov <petarstefanov@ymail.com>
2017-08-23 19:57:48 +0100

    Roll a ball game with fully integrated Astra Api
    under prototype folder all c# files along with built game - exe file

---
+++ prototype/ScriptsRollABall/Directions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssemblyCSharp
+{
+	[Serializable]
+	public class Directions
+	{
+		public Directions ()
+		{
+		}
+
+		public float horizontal;
+		public float vertical;
+		public string type;
+	}
+}
+
---
+++ prototype/ScriptsRollABall/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AssemblyCSharp;
+
+
+public class PlayerController : MonoBehaviour
+{
+	public Text countText;
+	public Text winText;
+	public Text astraText;
+	private Rigidbody rb;
+	private int count;
+	public float speed;
+	public string update;
+	float moveHorizontal = 0.0f;
+	float moveVertical = 0.0f;
+
+	api.AstraApiImpl apiAstra = new api.AstraApiImpl ();
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		string name = apiAstra.createAgent ("unity_player", "Player");
+		print ("Agent name: " + name);
+		count = 0;
+		SetCountText ();
+		astraText.text = "Astra responded: ";
+		winText.text = "";
+		SetPosition ("Default");  
+	}
+
+	private void SetPosition (string update)
+	{
+		string directionsFromAstra = apiAstra.syncEvent ("position", new object[] { update });
+
+		Directions directions = new Directions ();
+		directions = JsonUtility.FromJson<Directions> (directionsFromAstra);
+		astraText.text = "Astra responded: " + " X: " + directions.horizontal + " Y: " + directions.vertical;
+
+		moveHorizontal = directions.horizontal;
+		moveVertical = directions.vertical;
+	}
+
+	void FixedUpdate ()
+	{
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		if (other.gameObject.CompareTag ("EastWall")) {
+			other.gameObject.SetActive (true);
+			SetPosition ("HorizontalRight");
+		} else if (other.gameObject.CompareTag ("WestWall")) {
+			other.gameObject.SetActive (true);
+			SetPosition ("HorizontalLeft");
+		} else if (other.gameObject.CompareTag ("SouthWall")) {
+			other.gameObject.SetActive (true);
+			SetPosition ("VerticalUp");
+
+		} else if (other.gameObject.CompareTag ("NorthWall")) {
+			other.gameObject.SetActive (true);
+			SetPosition ("VerticalDown");
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count > 11) {
+			winText.text = "Winner";
+		}
+       
+	}
+}
